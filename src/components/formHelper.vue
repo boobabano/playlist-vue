d7a2f72999621c9f71941d16ea0416741ed6d4e5
boobabano <39481803+boobabano@users.noreply.github.com>
2020-07-09 13:34:37 -0400

    first commit

---
+++ src/components/formHelper.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>I am the form helper</h1>
+</template>
+
+<script>
+
+
+export default {
+  components:{
+
+  },
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/showBlogs.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <div id="show-blogs">
+    <h1>All Blogs Articles</h1>
+    <input type="text" v-model="search" placeholder="searchBlogs">
+    <div v-for="blog in filteredBlogs" class="single-blog">
+      <h2>{{ blog.title|to-uppercase }}</h2>
+      <article>{{ blog.body|snippet }}</article>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  components:{
+
+  },
+  data(){
+    return {
+      blogs:[],
+      search:''
+    }
+  },
+  methods:{
+
+  },
+  created(){
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+      this.blogs=data.body.slice(0,10);
+    })
+  },
+  computed:{
+    filteredBlogs:function(){
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+#show-blogs{
+  margin: 0 auto;
+  max-width:800px;
+}
+
+.single-blog{
+  padding:20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+
+
+</style>
